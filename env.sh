4854e8284979f0e66ed21f58227e1866e88ce11c
popeye the sailor <thepopsai@gmail.com>
2021-10-08 18:06:24 +0100

    updated env build script

--- env.sh
+++ env.sh
@@ -10,6 +10,7 @@ sudo apt-get install gcc g++ make
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn && yarn --version
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 cd ~/metaplex/js
 yarn install
 yarn bootstrap
